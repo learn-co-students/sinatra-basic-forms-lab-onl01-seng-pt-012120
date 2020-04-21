0ab114d713f01dfb214a5e89d33e541794bcbd99
James Batsell <jamesbatsell05@gmail.com>
2020-04-21 05:02:49 +0000

    Done.

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,10 +1,11 @@
 class Puppy
-  
+
   attr_accessor :name, :breed, :age
-  
-  def initialize(name, breed, age)
+
+  def initialize(name,breed,age)
     @name = name
     @breed = breed
-    @age = age
+    @age = age 
   end
+
 end
